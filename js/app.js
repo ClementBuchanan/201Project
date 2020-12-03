2090e6cb4f140b47fe0a03cc3d7beeaa8cb277fd
ClementBuchanan <clement.buchanan@gmail.com>
2020-12-02 21:31:13 -0800

    made complete app.js and edited index.html files

--- js/app.js
+++ js/app.js
@@ -1,50 +1,66 @@
-'use strict'
+// 'use strict'
 
-var userName = prompt('What is your name?');
-alert('Hello ' + userName);
+// var userName = prompt('What is your name?');
+// alert('Hello ' + userName);
 
-// Question One
-let bio = prompt('Please answer yes/no or y/n to my questions. Have you read my bio?');
-bio.toLowerCase;
-if ((bio === 'yes') || (bio === 'y')){
-  alert('That\'s Greeeaaaat! Now answer the next question.');
-} else {
-  alert('Wrong answer');
+// // Question One
+// let bio = prompt('Please answer yes/no or y/n to my questions. Have you read my bio?');
+// bio.toLowerCase;
+// if ((bio === 'yes') || (bio === 'y')){
+//   alert('That\'s Greeeaaaat! Now answer the next question.');
+// } else {
+//   alert('Wrong answer');
+// }
+
+// // Question two
+// let city = prompt('Did I grow up in Brooklyn, NY?');
+// city.toLowerCase;
+// if ((city === 'yes') || (city === 'y')){
+//   alert('That\'s correct ' + userName + '. Thank you for knowing this. Move on to the next question.');
+// } else {
+// alert('Wrong answer');
+// }
+
+// // Question three
+// let vet = prompt('Am I a veteran ' + userName + '?');
+// vet.toLowerCase;
+// if ((vet === 'yes') || (vet === 'y')){
+//   alert('That\'s correct ' + userName + ' you do know more things about me. Move on to the next question.');
+// } else {
+// alert('Wrong answer');
+// }
+
+// // Question four
+// let work = prompt('Do I work in public health ' + userName + '?');
+// work.toLowerCase;
+// if ((work === 'yes') || (work === 'y')){  
+//   alert('That\'s correct ' + userName + ' you really know a lot about me. Move on to the next question.');
+// } else {
+//   alert('Wrong answer');
+// }
+
+// // Question five
+// let future = prompt('Alright then ' + userName  + ' would you believe me if I told you I\'m from the future?');
+// future.toLowerCase;
+// if ((future === 'yes') || (future === 'y')){  
+//   alert('Yowza ' + userName + ' you really are smart.');
+// } else {
+//   alert('Wrong answer');
+// }
+// alert('Thank you for playing this game ' + userName + '. You\'ve been a good sport.');
+
+alert('You have 5 guesses. Guess a number between 1 and 10');
+
+var answer = 8;
+var guess = prompt('So what\'s your guess?');
+
+for (i = 0; i < 4; i++ ){
+    if (answer == guess){
+      alert('You guessed correctly');
+        break;
+    } else {
+     guess = prompt('Wrong answer. Please try again'); 
 }
-
-// Question two
-let city = prompt('Did I grow up in Brooklyn, NY?');
-city.toLowerCase;
-if ((city === 'yes') || (city === 'y')){
-  alert('That\'s correct ' + userName + '. Thank you for knowing this. Move on to the next question.');
-} else {
-alert('Wrong answer');
+    console.log(i);
 }
-
-// Question three
-let vet = prompt('Am I a veteran ' + userName + '?');
-vet.toLowerCase;
-if ((vet === 'yes') || (vet === 'y')){
-  alert('That\'s correct ' + userName + ' you do know more things about me. Move on to the next question.');
-} else {
-alert('Wrong answer');
-}
-
-// Question four
-let work = prompt('Do I work in public health ' + userName + '?');
-work.toLowerCase;
-if ((work === 'yes') || (work === 'y')){  
-  alert('That\'s correct ' + userName + ' you really know a lot about me. Move on to the next question.');
-} else {
-  alert('Wrong answer');
-}
-
-// Question five
-let future = prompt('Alright then ' + userName  + ' would you believe me if I told you I\'m from the future?');
-future.toLowerCase;
-if ((future === 'yes') || (future === 'y')){  
-  alert('Yowza ' + userName + ' you really are smart.');
-} else {
-  alert('Wrong answer');
-}
-alert('Thank you for playing this game ' + userName + '. You\'ve been a good sport.');
+alert('Thank you for playing');
