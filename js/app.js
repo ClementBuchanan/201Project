c22169396005abd3ce41fecb35b48a164a86f045
ClementBuchanan <clement.buchanan@gmail.com>
2020-12-07 10:06:31 -0800

    made updated changes for resumbission

--- js/app.js
+++ js/app.js
@@ -1,116 +1,83 @@
 'use strict';
 
-function usr() {
   var userName = prompt('What is your name?');
   alert('Hello ' + userName);
-}
-usr();
 
-// Question One
-function biology() {
+
+// Question 1
   let bio = prompt('Please answer yes/no or y/n to my questions. Have you read my bio?');
   bio.toLowerCase;
   if ((bio === 'yes') || (bio === 'y')) {
     alert('That\'s Greeeaaaat! Now answer the next question.');
   } else {
-    alert('You need to read my bio');
+    alert('Wrong answer');
   }
-}
-biology();
 
-// Question two
-function city1() {
-  let city = prompt('Did I grow up in Brooklyn, NY?');
-  city.toLowerCase;
-  if ((city === 'yes') || (city === 'y')) {
-    alert('That\'s correct ' + userName + '. Thank you for knowing this. Move on to the next question.');
-  } else {
-    alert('Wrong answer. You have to read it ' + userName + '.');
-  }
-}
-city1();
-
-// Question three
-function veteran() {
-  let vet = prompt('Am I a veteran ' + userName + '?');
-  vet.toLowerCase;
-  if ((vet === 'yes') || (vet === 'y')) {
-    alert('That\'s correct ' + userName + ' you do know more things about me. Move on to the next question.');
-  } else {
-    alert('Awe jeez that\'s the wrong answer again.');
-  }
-}
-veteran();
-
-// Question four
-function work2() {
-  let work = prompt('Do I work in public health ' + userName + '?');
-  work.toLowerCase;
-  if ((work === 'yes') || (work === 'y')) {
-    alert('That\'s correct ' + userName + ' you really know a lot about me. Move on to the next question.');
-  } else {
-    alert('Hmm!! Please read it.');
-  }
-}
-work2();
-
-// Question five
-function future2() {
-  let future = prompt('Alright then ' + userName + ' would you believe me if I told you I\'m from the future?');
-  future.toLowerCase;
-  if ((future === 'yes') || (future === 'y')) {
-    alert('Yowza ' + userName + ' you really are smart.');
-  } else {
-    alert('That\'s still the wrong answer');
-  }
-}
-future2();
-
-// Question 6
-function alert2() {
-  alert('You will now have five chances to guess a number between 1 and 10');
-}
-alert2();
-
-function answer2() {
-  var guessNumber = 5;
-  var myNumber = 7;
-  for (var j = 0; j < guessNumber; j++) {
-  var userNumber = prompt('I\'m thinking of a number between 1 and 10. Guess what number it is?');
-    if (userNumber > myNumber){
-      alert('Your guess is too high. Please try again.');
-    } else if (userNumber < myNumber){
-    alert('You\'re too low. Please try again.');
-    } else if (userNumber == myNumber) {
-    alert('Ding, Ding, Ding you\'re a winner.');
-    break;
+  // Question 2
+    let city = prompt('Did I grow up in Brooklyn, NY?');
+    city.toLowerCase;
+    if ((city === 'yes') || (city === 'y')) {
+      alert('That\'s correct ' + userName + '. Thank you for knowing this. Move on to the next question.');
+    } else {
+      alert('Wrong answer');
     }
-  }
-    alert('I was thinking of ' + myNumber + '. Thank you for playing ' + userName + '.');
-  }
-answer2();
 
-// Question 7
-alert('Lastly, you will have 6 chances to guess a color');
+    // Question 3
+      let vet = prompt('Am I a veteran ' + userName + '?');
+      vet.toLowerCase;
+      if ((vet === 'yes') || (vet === 'y')) {
+        alert('That\'s correct ' + userName + ' you do know more things about me. Move on to the next question.');
+      } else {
+        alert('Wrong answer');
+      }
 
-function colors2() {
-var guess = prompt('Think of a color. Any color.');
-var colors = ['fusia', 'dark grey', 'blood red', 'orange', 'green', 'pink', 'sky blue'];
-var guesses = 6;
-var correctAnswer = false;
+      // Question 4
+        let work = prompt('Do I work in public health ' + userName + '?');
+        work.toLowerCase;
+        if ((work === 'yes') || (work === 'y')) {
+          alert('That\'s correct ' + userName + ' you really know a lot about me. Move on to the next question.');
+        } else {
+          alert('Wrong answer');
+        }
 
-console.log('colors');
+        // Queestion 5
+          let future = prompt('Alright then ' + userName + ' would you believe me if I told you I\'m from the future?');
+          future.toLowerCase;
+          if ((future === 'yes') || (future === 'y')) {
+            alert('Yowza ' + userName + ' you really are smart.');
+          } else {
+            alert('Wrong answer');
+          }
 
-for (var correctAnswer = ('fuscia');
-  for (var k = 0; k < 6; k++ ) {
-    if (guess == colors[1]){
-      alert('You guessed correctly. The correct color was ' + correctAnswer + '.');
-      break;
-  } else {
-   guess = prompt('Wrong color. Please try again'); 
-}
-  console.log(j);
-} 
-alert('Thank you for playing this game ' + userName + '. You\'ve exceeded the number of attempts. The correct answer was '+ answer + ' You were a good sport.');
-}
-colors2();
+          // Question 6
+            alert('You will now have five chances to guess a number.');
+            var guessNumber = 5;
+            var myNumber = 7;
+            for (var j = 0; j < guessNumber; j++) {
+              var userNumber = prompt('I\'m thinking of a number between 1 and 10. Guess what number it is?');
+              if (userNumber > myNumber) {
+                alert('Your guess is too high. Please try again.');
+              } else if (userNumber < myNumber) {
+                alert('You\'re too low. Please try again.');
+              } else if (userNumber == myNumber) {
+                alert('Ding, Ding, Ding you\'re a winner.');
+                break;
+              }
+            }
+            alert('I was thinking of ' + myNumber + '. Thank you for playing ' + userName + '.');
+
+          // Question 7
+            alert('Lastly, you will have 6 chances to guess a color.');
+            var colors = ['fusia', 'orange', 'green', 'pink', 'sky blue'];
+            var correctAnswer = 'fusia';
+            var guessNumber = 6;
+            for (var k = 0; k < 6; k++) {
+              var guess = prompt('Think of a color. Any color.');
+              if (guess == colors[0]) {
+                alert('You guessed correctly. The correct color was ' + correctAnswer + '.');
+                break;
+              } else if (guess !== correctAnswer) {
+                guess = prompt('Wrong color. Please try again');
+              }
+            }
+            alert('Thank you for playing this game ' + userName + '. You\'ve exceeded the number of attempts. The correct answer was ' + correctAnswer + ' You were a good sport.');
--- js/test.js
+++ js/test.js
@@ -1,76 +1,114 @@
 'use strict';
 
-var userName = prompt('What is your name?');
-
-alert('Hello ' + userName);
+function usr() {
+  var userName = prompt('What is your name?');
+  alert('Hello ' + userName);
+}
+usr();
 
 // Question One
-let bio = prompt('Please answer yes/no or y/n to my questions. Have you read my bio?');
-bio.toLowerCase;
-if ((bio === 'yes') || (bio === 'y')){
-  alert('That\'s Greeeaaaat! Now answer the next question.');
-} else {
-  alert('Wrong answer');
-}
-// Question two
-let city = prompt('Did I grow up in Brooklyn, NY?');
-city.toLowerCase;
-
-if ((city === 'yes') || (city === 'y')){
-  alert('That\'s correct ' + userName + '. Thank you for knowing this. Move on to the next question.');
-} else {
-alert('Wrong answer'); 
-}
-// Question three
-let vet = prompt('Am I a veteran ' + userName + '?');
-vet.toLowerCase;
-if ((vet === 'yes') || (vet === 'y')){
-  alert('That\'s correct ' + userName + ' you do know more things about me. Move on to the next question.');
-} else {
-alert('Wrong answer');
-}
-let work = prompt('Do I work in public health ' + userName + '?');
-work.toLowerCase;
-if ((work === 'yes') || (work === 'y')){
-  alert('That\'s correct ' + userName + ' you really know a lot about me. Move on to the next question.');
-} else {
-  alert('Wrong answer');
-}
-let future = prompt('Alright then ' + userName  + ' would you believe me if I told you I\'m from the future?');
-future.toLowerCase;
-if ((future === 'yes') || (future === 'y')){
-  alert('Yowza ' + userName + ' you really are smart.');
-} else {
-  alert('Wrong answer');
-}
-// Question four
-alert('You will now have five chances to guess a number.');
-var guessNumber = 5;
-var myNumber = 7;
-for (var j = 0; j < guessNumber; j++) {
-var userNumber = prompt('I\'m thinking of a number between 1 and 10. Guess what number it is?');
-  if (userNumber > myNumber){
-    alert('Your guess is too high. Please try again.');
-  } else if (userNumber < myNumber){
-  alert('You\'re too low. Please try again.');
-  } else if (userNumber == myNumber) {
-  alert('Ding, Ding, Ding you\'re a winner.');
-  break;
+function biology() {
+  let bio = prompt('Please answer yes/no or y/n to my questions. Have you read my bio?');
+  bio.toLowerCase;
+  if ((bio === 'yes') || (bio === 'y')) {
+    alert('That\'s Greeeaaaat! Now answer the next question.');
+  } else {
+    alert('You need to read my bio');
   }
 }
-//question five
-  alert('I was thinking of ' + myNumber + '. Thank you for playing ' + userName + '.');
-alert('Lastly, you will have 6 chances to guess a color.');
-var colors = ['fusia', 'orange', 'green', 'pink', 'sky blue'];
-var correctAnswer = 'fusia';
-var guessNumber = 6;
-for (var k = 0; k < 6; k++ ){
-  var guess = prompt('Think of a color. Any color.');
-    if (guess == colors[0]){
+biology();
+
+// Question two
+function city1() {
+  let city = prompt('Did I grow up in Brooklyn, NY?');
+  city.toLowerCase;
+  if ((city === 'yes') || (city === 'y')) {
+    alert('That\'s correct ' + userName + '. Thank you for knowing this. Move on to the next question.');
+  } else {
+    alert('Wrong answer. You have to read it ' + userName + '.');
+  }
+}
+city1();
+
+// Question three
+function veteran() {
+  let vet = prompt('Am I a veteran ' + userName + '?');
+  vet.toLowerCase;
+  if ((vet === 'yes') || (vet === 'y')) {
+    alert('That\'s correct ' + userName + ' you do know more things about me. Move on to the next question.');
+  } else {
+    alert('Awe jeez that\'s the wrong answer again.');
+  }
+}
+veteran();
+
+// Question four
+function work2() {
+  let work = prompt('Do I work in public health ' + userName + '?');
+  work.toLowerCase;
+  if ((work === 'yes') || (work === 'y')) {
+    alert('That\'s correct ' + userName + ' you really know a lot about me. Move on to the next question.');
+  } else {
+    alert('Hmm!! Please read it.');
+  }
+}
+work2();
+
+// Question five
+function future2() {
+  let future = prompt('Alright then ' + userName + ' would you believe me if I told you I\'m from the future?');
+  future.toLowerCase;
+  if ((future === 'yes') || (future === 'y')) {
+    alert('Yowza ' + userName + ' you really are smart.');
+  } else {
+    alert('That\'s still the wrong answer');
+  }
+}
+future2();
+
+// Question 6
+function alert2() {
+  alert('You will now have five chances to guess a number between 1 and 10');
+}
+alert2();
+
+function answer2() {
+  var guessNumber = 5;
+  var myNumber = 7;
+  for (var j = 0; j < guessNumber; j++) {
+  var userNumber = prompt('I\'m thinking of a number between 1 and 10. Guess what number it is?');
+    if (userNumber > myNumber){
+      alert('Your guess is too high. Please try again.');
+    } else if (userNumber < myNumber){
+    alert('You\'re too low. Please try again.');
+    } else if (userNumber == myNumber) {
+    alert('Ding, Ding, Ding you\'re a winner.');
+    break;
+    }
+  }
+    alert('I was thinking of ' + myNumber + '. Thank you for playing ' + userName + '.');
+  }
+answer2();
+
+// Question 7
+alert('Lastly, you will have 6 chances to guess a color');
+
+function colors2() {
+var guess = prompt('Think of a color. Any color.');
+var colors = ['fusia', 'dark grey', 'blood red', 'orange', 'green', 'pink', 'sky blue'];
+var guesses = 6;
+var correctAnswer = false;
+//console.log('colors');
+for (var correctAnswer = ('fuscia');
+  for (var k = 0; k < 6; k++) {
+    if (guess == colors[1]){
       alert('You guessed correctly. The correct color was ' + correctAnswer + '.');
       break;
-  } else if (guess !== correctAnswer){
-     guess = prompt('Wrong color. Please try again'); 
-  }
+  } else {
+   guess = prompt('Wrong color. Please try again'); 
 }
-alert('Thank you for playing this game ' + userName + '. You\'ve exceeded the number of attempts. The correct answer was '+ correctAnswer + ' You were a good sport.');
+  console.log(j);
+} 
+alert('Thank you for playing this game ' + userName + '. You\'ve exceeded the number of attempts. The correct answer was '+ answer + ' You were a good sport.');
+}
+colors2();
